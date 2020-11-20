2deb295e8057213f91498fbe3a29a375deb5bff4
joseph-beep <74767414+joseph-beep@users.noreply.github.com>
2020-11-20 10:52:18 +0100

    Uploaded files.

---
+++ javascript.js
@@ -0,0 +1,15 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var i = 0;
+
+window.setInterval(() => {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+	ctx.fillStyle = "#FF0000";
+	ctx.fillRect(0 + i, 0, 150, 75);
+
+	ctx.fill
+	
+	i++;
+}, 60);
