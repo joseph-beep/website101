9fb1692c56db41d22855f7d5c8893d826dcb8eb6
joseph-beep <74767414+joseph-beep@users.noreply.github.com>
2020-11-23 11:42:20 +0100

    Applying CSS

--- javascript.js
+++ javascript.js
@@ -1,15 +1,17 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+// var canvas = document.getElementById("canvas");
+// var ctx = canvas.getContext("2d");
 
-var i = 0;
+// var i = 0;
 
-window.setInterval(() => {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+// window.setInterval(() => {
+    // ctx.clearRect(0,0,canvas.width,canvas.height);
 
-	ctx.fillStyle = "#FF0000";
-	ctx.fillRect(0 + i, 0, 150, 75);
+	// ctx.fillStyle = "#FF0000";
+	// ctx.fillRect(0 + i, 0, 150, 75);
 
-	ctx.fill
+	// ctx.fill
 	
-	i++;
-}, 60);
+	// i++;
+	
+	// if (event.button != undefined) alert("You pressed button: " + event.button)
+// }, 60);
